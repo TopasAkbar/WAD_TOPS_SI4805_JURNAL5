c058d615e864e4088e64201dea3c46c637ad4013
KresnaP <pebriawangg2@gmail.com>
2025-12-04 16:02:32 +0700

    JURNAL MODUL 5 Cassette

--- app/Http/Controllers/CassetteController.php
+++ app/Http/Controllers/CassetteController.php
@@ -17,9 +17,10 @@ class CassetteController extends Controller
     {
         // ambil semua data cassette
         // $cassettes = ....
-
+        $cassettes = Cassette::all();
         // return koleksi cassette
         // return ....
+        return CassetteResource::collection($cassettes);
     }
 
     /**
@@ -30,21 +31,30 @@ class CassetteController extends Controller
     {
         // Request body berisi title, artist dan year
         $validator = Validator::make($request->all(), [
-            
+            'title' => 'required|string|max:255',
+            'artist' => 'required|string|max:255',
+            'year' => 'required|year',
         ]);
 
         if ($validator->fails()) {
             return response()->json([
                 // 'success' => false,
                 // 'errors' => ....
+                'success' => false,
+                'errors' => 'Please check your request'
             ], 422);
         }
 
         // Buat data cassette
         // $cassette = ....
+        $cassette = Cassette::create($validator->validated());
 
         // return cassette yang dibuat sebagai resource
         // return ....
+        return (new CassetteResource($cassette))
+                    ->additional(['message' => 'Cassette created successfully!'])
+                    ->response()
+                    ->setStatusCode(201);
 
     }
 
@@ -56,16 +66,20 @@ class CassetteController extends Controller
     {
         // Cari data cassette berdasarkan ID
         // $cassette = ....
+        $cassette = Cassette::find($id);
 
         if (!$cassette) {
             return response()->json([
                 // 'success' => false,
                 // 'message' => ....
+                'success' => false,
+                'message' => 'Cassette not found'
             ], 404);
         }
 
         // return cassette sebagai resource
         // return ....
+        return new CassetteResource($cassette);
     }
 
     /**
@@ -76,16 +90,21 @@ class CassetteController extends Controller
     {
         // Request body berisi title, artist dan year
         $validator = Validator::make($request->all(), [
-            
+            'title' => 'required|string|max:255',
+            'artist' => 'required|string|max:255',
+            'year' => 'required|year',
         ]);
 
         // Cari data cassette berdasarkan ID
-        // $cassette = ....
+        // $cassette = ....\
+        $cassette = Cassette::find($id);
 
         if (!$cassette) {
             return response()->json([
                 // 'success' => false,
                 // 'message' => ....
+                'seccess' => false,
+                'message' => 'Cassette not found'
             ], 404);
         }
 
@@ -94,14 +113,21 @@ class CassetteController extends Controller
             return response()->json([
                 // 'success' => false,
                 // 'errors' => ....
+                'success' => false,
+                'errors' => $validator->errors()
             ], 422);
         }
 
         // Update data cassette
         // $cassette->....
+        $cassette->update($validator->validated());
 
         // return cassette yang diupdate sebagai resource
         // return ....
+        return (new CassetteResource($cassette))
+                    ->additional(['message' => 'Cassette updated successfully!'])
+                    ->response()
+                    ->setStatusCode(200);
     }
 
     /**
@@ -112,18 +138,23 @@ class CassetteController extends Controller
     {
         // Cari data cassette berdasarkan ID
         // $cassette = ....
+        $cassette = Cassette::find($id);
 
         if (!$cassette) {
             return response()->json([
                 // 'success' => false,
                 // 'message' => ....
+                'success' => false,
+                'message' => 'Cassette not found'
             ], 404);
         }
 
         // Hapus data cassette
-        // $cassette->....
+        // $cassette->....\
+        $cassette->delete();
 
         // return message sukses
         // return ....
+        return response()->json(['message' => 'Cassette deleted successfully!'], 200);
     }
 }
